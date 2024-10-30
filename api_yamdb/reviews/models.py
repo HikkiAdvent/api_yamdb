9cd30e60678f69fe121cd6b64a9d2f452332e2b8
Sergei Chistiakov <sway-wow@yandex.ru>
2024-10-30 15:49:54 +0300

    add Comment model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Импорт Title и User
-
 
 class Review(models.Model):
     title = models.ForeignKey(
@@ -31,6 +29,34 @@ class Review(models.Model):
     class Meta:
         verbose_name = 'Отзыв',
         verbose_name_plural = 'Отзывы',
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    author = models.ForeignKey(
+        User, # Ссылка на модель User
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий',
+        verbose_name_plural = 'Комментарии'
 
     def __str__(self):
         return self.text
