03a3e3fa607878d06f82da1d30371f56c400f187
Sergei Chistiakov <sway-wow@yandex.ru>
2024-10-30 14:45:42 +0300

    add Reviews model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Импорт Title и User
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, # Ссылка на модель Title
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User, # Ссылка на модель User
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    text = models.TextField()
+    score = models.IntegerField(
+        'Оценка',
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    ),
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв',
+        verbose_name_plural = 'Отзывы',
+
+    def __str__(self):
+        return self.text
