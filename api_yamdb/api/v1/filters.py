0dbff6da0faca9c8bd23df2a28d37d9b465e8e52
bvv-praktikum <bvv2003@yandex.ru>
2024-11-05 23:10:34 +0400

    edit filter

--- api_yamdb/api/v1/filters.py
+++ api_yamdb/api/v1/filters.py
@@ -6,23 +6,11 @@ from reviews.models import Title
 class TitleFilter(filters.FilterSet):
     """Фильтр выборки произведений по определенным полям."""
 
-    category = filters.CharFilter(
-        field_name='category__slug',
-        lookup_expr='icontains'
-    )
-    genre = filters.CharFilter(
-        field_name='genre__slug',
-        lookup_expr='icontains'
-    )
-    name = filters.CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    )
-    year = filters.NumberFilter(
-        field_name="year",
-        lookup_expr='exact'
-    )
-
     class Meta:
         model = Title
-        fields = ('category', 'genre', 'name', 'year')
+        filterset_fields = {
+            'category__slug': ['icontains'],
+            'genre__slug': ['icontains'],
+            'name': ['icontains'],
+            'year': ['exact'],
+        }
