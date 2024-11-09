8910e2f4ffa9f61d954fc8d75b0c8b099097e4a0
Valerij <valerijpetrencko@gmail.com>
2024-11-09 14:15:35 +0300

    Доработкана админка

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -56,21 +56,18 @@ class TitleAdmin(admin.ModelAdmin):
     list_per_page = LIST_PER_PAGE
     search_fields = ('name', 'year', 'category')
 
+    @admin.display(description='Жанр/ы произведения')
     def get_genre(self, object):
         """Получает жанр или список жанров произведения."""
         return '\n'.join((genre.name for genre in object.genre.all()))
 
-    get_genre.short_description = 'Жанр/ы произведения'
-
+    @admin.display(description='Количество отзывов')
     def count_reviews(self, object):
         """Вычисляет количество отзывов на произведение."""
         return object.reviews.count()
 
-    count_reviews.short_description = 'Количество отзывов'
-
+    @admin.display(description='Рейтинг')
     def get_rating(self, object):
         """Вычисляет рейтинг произведения."""
         rating = object.reviews.aggregate(average_score=Avg('score'))
         return round(rating.get('average_score'), 1)
-
-    get_rating.short_description = 'Рейтинг'
