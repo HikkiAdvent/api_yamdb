8efd2119d077c35b26639f231b89ffd274871bee
HikkiAdvent <valerpetrencko@yandex.ru>
2024-11-17 17:21:56 +0300

    Доработан пользователь

--- api_yamdb/api/v1/reviews/utils.py
+++ api_yamdb/api/v1/reviews/utils.py
@@ -4,6 +4,7 @@ from reviews.models import Title, Review
 
 def get_title_or_review(title_id, review_id=None):
     """Получиает объект или возрвращает 404."""
+
     title = get_object_or_404(Title, id=title_id)
     if review_id:
         return get_object_or_404(Review, id=review_id, title=title)
